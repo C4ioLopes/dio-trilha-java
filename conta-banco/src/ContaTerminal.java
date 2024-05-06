88d3a68c7a2b297b86ebf87d89abf93f8d026e78
Caio Silva Lopes <itzcaio2005@gmail.com>
2024-05-06 19:38:25 -0300

    Termino-da-Tarefa

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Por favor, digite o número do Banco !");
+    int numero = scanner.nextInt();
+
+    String agencia = "067-8";
+    
+    System.out.println("Digite o Nome do Cliente !");
+    String nome = scanner.next();
+
+    float saldo = (float) 237.48;
+
+    System.out.println("Olá " + nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta "+ numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
